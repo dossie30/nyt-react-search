e278315d1ca4dd978d923956b7db74f6e1c9b533
Dawn Tossie <dawn.tossie@gmail.com>
2018-06-30 01:21:26 -0400

    routes done

---
+++ routes/api/index.js
@@ -0,0 +1,11 @@
+const router = require("express").Router();
+
+const articleRoutes = require("./articles/articleRoutes");
+
+const nytRoutes = require("./nyt/nytRoutes");
+
+router.use("/articles", articleRoutes);
+
+router.use("/nyt", nytRoutes);
+
+module.exports = router;
